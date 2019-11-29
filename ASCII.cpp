5f77893b95d87a2d5aa1d18fbc27eaa4a2a4a507
Nimesh Srivastava <30381993+Nimesh-Srivastava@users.noreply.github.com>
2019-11-29 09:34:27 +0530

    Add files via upload

---
+++ ASCII.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+
+using namespace std;
+
+void main()
+{
+	system("cls");
+	char a;
+	int b;
+	cout<<"Enter a character : ";
+	cin>>a;
+	b = int(a);
+	cout<<"ASCII value : "<<b;
+	(void)_getch();
+}
---
+++ difference.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+
+using namespace std;
+
+void main()
+{
+	system("cls");
+	int pp,sp;
+	cout<<"Enter purchase price : ";
+	cin>>pp;
+	cout<<"Enter selling price : ";
+	cin>>sp;
+	cout<<"Difference is : "<<sp-pp;
+}
