11c29fc8a755cbff6c6b0f20c552fb3f5618af2a
Nimesh Srivastava <30381993+Nimesh-Srivastava@users.noreply.github.com>
2019-12-11 00:27:15 +0530

    Add files via upload

---
+++ remainder.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+
+using namespace std;
+
+void main()
+{
+	system("cls");
+	int a,b;
+	cout<<"Enter a : ";
+	cin>>a;
+	cout<<"Enter b : ";
+	cin>>b;
+	cout<<"Remainder after dividing a by b : "<<a%b;
+}
